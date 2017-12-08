fc8f3172c8810602cda6c059833d04f12b35cd28
ben.p <polanskyb@gmail.com>
2017-12-08 11:08:04 -0800

    integrate semantic ui

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
 
-const ButtonExampleEmphasis = () => (
-  <div>
-    <Button primary>Primary</Button>
-    <Button secondary>Secondary</Button>
-  </div>
-)
-
 const App = () => (
   <div>
-    <h1>hello!</h1>
+    <Button
+      color='orange'
+      size='massive'
+    >
+      Hello!
+    </Button>
   </div>
 )
 
