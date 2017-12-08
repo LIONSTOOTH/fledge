f7d8513cd8584cc19a1124f4aec0142dccd5c73e
Krista <krista.abraham@gmail.com>
2017-12-08 01:31:16 -0500

    Modified config input to render

--- client/index.js
+++ client/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import App from './components/App'
-import reducer from './reducers'
+import App from './components/app.jsx'
+import reducer from './reducers/main.jsx'
 
 const store = createStore(reducer)
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/components/app.jsx',
+  entry: './client/index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'bundle.js'
