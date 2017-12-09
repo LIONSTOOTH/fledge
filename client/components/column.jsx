4754326e4c633053aad99581b4ee6988a2e3f3f1
Krista <krista.abraham@gmail.com>
2017-12-08 22:35:23 -0500

    Render one column within dashboard/kanban

--- client/components/column.jsx
+++ client/components/column.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ApplicationChip from '../components/applicationChip.jsx'
+import ApplicationChip from '../components/applicationChip.jsx';
 
 export const Column = ({ applications }) => {
+  console.log('in column', applications)
   return(
     <div>
     <h1>{applications.applications[0].status}</h1>
     <ul>
-      {applications.applications.map(application =>
+      {applications.applications.map((application) =>
         <ApplicationChip
           key={application.id}
           id={application.id}
