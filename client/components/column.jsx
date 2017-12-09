ef0fc14262a0df7afbef2e751cb12d1b036fe255
Krista <krista.abraham@gmail.com>
2017-12-09 15:11:52 -0500

    Refactored column to presentational

--- client/components/column.jsx
+++ client/components/column.jsx
@@ -1,41 +1,20 @@
 import React from 'react';
 import ApplicationChip from '../components/applicationChip.jsx';
-import { getAllApplications } from '../actions/jobApplications.jsx';
-import { connect } from 'react-redux';
-
 
 export const Column = ({ title, applications }) => {
-  console.log('applications in column', applications)
   return(
     <div style={{ border: 'solid 3px red' }}>
-    <h1>{title}</h1>
-    <ul>
-      {applications.map((application) =>
-        <ApplicationChip
-          key={application.id}
-          id={application.id}
-          application={application}
-        />
-      )}
-    </ul>
-
-  </div>
-);
+      <h1>{title}</h1>
+      <ul>
+        {applications.map((application) =>
+          <ApplicationChip
+            key={application.id}
+            id={application.id}
+            application={application}
+          />
+        )}
+      </ul>
+    </div>
+  );
 }
 
-// dispatches an action
-const fetchApplicationsActionCreator = () => {
-  return {
-    type: 'FETCH_APPLICATIONS',
-    payload: getAllApplications(),
-  }
-}
-
-const mapStateToProps = (state) => {
-  console.log(state)
-  return {
-    applications: state.applicationReducer
-  };
-}
-
-export default connect(mapStateToProps, { fetchApplicationsActionCreator })(Column)
