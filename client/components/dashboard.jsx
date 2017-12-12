57578c4c8ddc31ee59e568e795c67bc36bd0c248
Krista <krista.abraham@gmail.com>
2017-12-11 23:42:21 -0500

    Cleaned up file

--- client/components/dashboard.jsx
+++ client/components/dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 import ApplicationModal from '../containers/applicationModal.jsx';
 import Kanban from '../containers/kanban.jsx';
 
