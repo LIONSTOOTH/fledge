323e69baefb196b099c5c62c043f1b9bcf0d5f52
Arthur Lee <arthurlee44@gmail.com>
2017-12-12 11:51:45 -0800

    Passed application data to modal form

--- client/containers/modalForm.jsx
+++ client/containers/modalForm.jsx
@@ -4,29 +4,33 @@ import { connect } from 'react-redux';
 import { Grid } from 'semantic-ui-react';
 import { Field, reduxForm } from 'redux-form';
 
-let ModalForm = props => {
-  const { handleSubmit, onSubmit } = props;
 
-    let context = this;
+class ModalForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
 
-  return (
-    <form onSubmit={handleSubmit(  () =>{console.log(reduxForm) }   )}>
-      <div>
-        <label htmlFor="firstName">Company Name</label>
-        <Field name="firstName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="lastName">Description</label>
-        <Field name="lastName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <Field name="email" component="input" type="email" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  )
+  render() {
+    const { handleSubmit, onSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(  () =>{console.log('hello applications', this.props.application) }   )}>
+        <div>
+          <label htmlFor="firstName">Company Name</label>
+          <Field name="firstName" component="input" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName">Description</label>
+          <Field name="lastName" component="input" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field name="email" component="input" type="email" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
 }
 
 ModalForm = reduxForm({
@@ -34,14 +38,29 @@ ModalForm = reduxForm({
   form: 'application'
 })(ModalForm)
 
+
+const getAllApplications = (user) => {
+  return (dispatch) => {
+    // dispatch a flag action to show waiting view
+    dispatch({ type: 'IS_FETCHING' })
+
+    const request = axios.get('/api/applications');
+
+    return request.then(
+      response => dispatch(fetchApplicationsSuccess(response.data.apps)))
+      .then(dispatch({ type: 'IS_FETCHING'}))
+      .catch(err => console.log(err));
+  }
+}
+
 const mapStateToProps = (state) => {
   return {
-    applications: state.applicationReducer.applications,
-    isFetching: state.fetchFlagReducer.isFetching,
+    applications: state.applicationReducer.applications
+
   };
 }
 
+export default connect(mapStateToProps, {getAllApplications})(ModalForm)
+
 // export default connect(mapStateToProps,
 //   { fetchApplicationsSuccess, getAllApplications })(Kanban);
-
-export default ModalForm;
