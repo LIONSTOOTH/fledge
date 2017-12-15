9c76ca2111ef40ace900e866bc6cb8ca34671cd2
Arthur Lee <arthurlee44@gmail.com>
2017-12-15 12:00:23 -0800

    Added google-auth-library

--- server/index.js
+++ server/index.js
@@ -127,6 +127,53 @@ app.listen(app.get('port'), () =>
   console.log('app running on port', app.get('port'))
 );
 
+// app.post('/api/reminder', (req, res) => {
+
+//   console.log('setting google calendar reminder', req.body)
+
+//     var event = {
+//   'summary': 'Google I/O 2015',
+//   'location': '800 Howard St., San Francisco, CA 94103',
+//   'description': 'A chance to hear more about Google\'s developer products.',
+//   'start': {
+//     'dateTime': '2017-12-15T09:00:00-07:00',
+//     'timeZone': 'America/Los_Angeles',
+//   },
+//   'end': {
+//     'dateTime': '2017-12-16T17:00:00-07:00',
+//     'timeZone': 'America/Los_Angeles',
+//   },
+//   'recurrence': [
+//     'RRULE:FREQ=DAILY;COUNT=2'
+//   ],
+//   'attendees': [
+//     {'email': 'lpage@example.com'},
+//     {'email': 'sbrin@example.com'},
+//   ],
+//   'reminders': {
+//     'useDefault': false,
+//     'overrides': [
+//       {'method': 'email', 'minutes': 24 * 60},
+//       {'method': 'popup', 'minutes': 10},
+//     ],
+//   },
+// };
+// var calendar = google.calendar('v3');
+// calendar.events.insert({
+//   auth: oauth2Client,
+//   calendarId: 'primary',
+//   resource: event,
+// }, function(err, event) {
+//   console.log('what the heck')
+//   if (err) {
+//     console.log('There was an error contacting the Calendar service: ' + err);
+//     return;
+//   }
+//   console.log('Event created: %s', event.htmlLink);
+// });
+
+
+// });
 
 /*
 // need to refactor client side logout
