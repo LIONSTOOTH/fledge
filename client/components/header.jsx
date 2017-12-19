5f64e5de287e5be1541a4b9dcaf13e7e1c245619
Arthur Lee <arthurlee44@gmail.com>
2017-12-19 10:07:49 -0800

    Added Google drive upload via google picker

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+<<<<<<< HEAD
 import { Button, Segment, Header, Icon } from 'semantic-ui-react';
+=======
+import { Button, Segment } from 'semantic-ui-react';
+import GooglePicker from './react-google-picker.jsx';
+
+>>>>>>> Added Google drive upload via google picker
 
 export const Head = ({ isLoggedIn, toggleVisibility }) => {
   return isLoggedIn ? (
@@ -18,6 +24,19 @@ export const Head = ({ isLoggedIn, toggleVisibility }) => {
           </a>
         </Button>
       </Segment>
+      <GooglePicker clientId={'108994268957-a7mgrj68ai43tdd89ivrsmuk4jcnhi0i.apps.googleusercontent.com'}
+              developerKey={'AIzaSyDdoVy5ZiRcUdnK_y171ocM4385IWaRbCg'}
+              scope={['https://www.googleapis.com/auth/drive']}
+              onChange={data => console.log('on change:', data)}
+              multiselect={true}
+              navHidden={true}
+              authImmediate={false}
+              mimeTypes={[]}
+              viewId={'DOCS'}
+
+      >
+
+</GooglePicker>
     </div>
   ) : (
     <div>
