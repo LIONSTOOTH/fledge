6e9b863d4376dadee1bf81074d20eeca22fd89cb
ben.p <polanskyb@gmail.com>
2017-12-19 10:58:01 -0800

    add contacts metrics materials reminders files and conditional rendering

---
+++ client/components/materials.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Materials = props => (
+  <div>
+    <h1>greetings from APPLICATION MATERIALS</h1>
+  </div>
+);
+
+export default Materials;
---
+++ client/components/reminders.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Reminders = props => (
+  <div>
+    <h1>greetings from REMINDERS</h1>
+  </div>
+);
+
+export default Reminders;
