03d01b2ed1d43ab0d663c541d9be18fbf55cf020
ben.p <polanskyb@gmail.com>
2018-01-02 11:47:55 -0800

    change some styling

--- client/components/congratsModal.jsx
+++ client/components/congratsModal.jsx
@@ -4,13 +4,10 @@ import { Button, Modal } from 'semantic-ui-react';
 const ModalExampleShorthand = () => (
   <Modal
     trigger={<Button>Show Modal</Button>}
-    header='Reminder!'
-    content='Call Benjamin regarding the reports.'
-    actions={[
-      'Snooze',
-      { key: 'done', content: 'Done', positive: true },
-    ]}
+    header="Reminder!"
+    content="Call Benjamin regarding the reports."
+    actions={['Snooze', { key: 'done', content: 'Done', positive: true }]}
   />
-)
+);
 
-export default ModalExampleShorthand
+export default ModalExampleShorthand;
